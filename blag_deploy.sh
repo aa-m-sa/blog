a45d62b46f609bd23961127956481caf78920237
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2016-07-04 00:53:48 +0300

    check that target env var is set

--- blag_deploy.sh
+++ blag_deploy.sh
@@ -2,12 +2,20 @@
 
 git checkout deploy
 
+set -e
+
 # build and deploy
 
 ./blag_build.sh
 
 # assumes env variable BLAG_DEPLOY_TGT
 
+if [ -z "$BLAG_DEPLOY_TGT"]
+    echo "needs target"
+    exit 1
+fi
+
+echo $BLAG_DEPLOY_TGT
 rsync -avz blog/_site/ $BLAG_DEPLOY_TGT
 
 # ...YEAAAH.
