f51817639885952b4a39a5305e591389a3a14194
Aaro Salosensaari <a.salosensaari@gmail.com>
2016-12-23 12:57:39 +0200

    clickable (show/hide) tag lists with jquery

---
+++ blog/scripts/categories.js
@@ -0,0 +1,14 @@
+
+function jq( myid ) {
+  return "#" + myid.replace( /(:|\.|\[|\]|,|=)/g, "\\$1" );
+}
+
+// clickable show/hide category list
+$( document ).ready(function() {
+  $( ".archive-tag-area" ).each( function(i, e) {
+    var taglist = $( this ).children( ".archive-tag-list" );
+    $( this ).children( ".archive-tag-clickable").on( 'click', function(event) {
+      taglist.toggle( "show" );
+    });
+  });
+})
